77c3e027509e5fd3098cf47b511de11319530418
Yusuph Omari <yousufummar649@gmail.com>
2024-03-14 13:50:01 +0300

    fixing user bug

--- src/common/database/database.module.ts
+++ src/common/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+import { DbMigrationService } from './db-migration.service';
 
 @Module({
   imports: [
@@ -11,9 +12,10 @@ import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
       inject: [ConfigService],
     }),
   ],
+  providers: [DbMigrationService],
 })
 export class DatabaseModule {
-  static forFeature(models:ModelDefinition[]){
-    return MongooseModule.forFeature(models)
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
   }
 }
