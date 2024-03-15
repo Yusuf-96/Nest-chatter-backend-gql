f04d30caee4d23afd4fbbfcdb808b3a1d4471a3e
Yusuph Omari <yousufummar649@gmail.com>
2024-03-15 09:36:29 +0300

    Jwt authentication functionality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { UsersRepository } from './users.repository';
@@ -28,8 +28,10 @@ export class UsersService {
   }
 
   async update(_id: string, updateUserInput: UpdateUserInput) {
-    if(updateUserInput.password){
-      updateUserInput.password = await this.hashPassword(updateUserInput.password);
+    if (updateUserInput.password) {
+      updateUserInput.password = await this.hashPassword(
+        updateUserInput.password,
+      );
     }
     return this.usersRepository.findOneAndUpdate(
       { _id },
@@ -44,5 +46,16 @@ export class UsersService {
 
   async remove(_id: string) {
     return this.usersRepository.findOneAndDelete({ _id });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    if (!passwordIsValid) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+
+    return user;
   }
 }
